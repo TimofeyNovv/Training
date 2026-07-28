00ad44034affdb06049e5b5e8566f3c49f8a63e3
TimofeyNov <timn2020@inbox.ru>
2026-07-28 18:35:46 +0300

    initial commit

---
+++ src/main/java/com/example/CargoFlow/CargoFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.CargoFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CargoFlow/CargoFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CargoFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
