6ebe637492bb3cc67e7dcb85d5b7be3690697dfe
TimofeyNov <timn2020@inbox.ru>
2026-07-30 15:34:52 +0300

    minor changes with config and structure

--- src/main/java/com/example/CargoFlow/demo/controller/DemoController.java
+++ src/main/java/com/example/CargoFlow/demo/controller/DemoController.java
@@ -1,20 +1,23 @@
-package com.example.CargoFlow.controller;
+package com.example.CargoFlow.demo.controller;
 
+import com.example.CargoFlow.demo.dto.PingResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class DemoController {
 
     @Operation(
             summary = "ping pong"
     )
     @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
-        return  ResponseEntity.ok("pong");
+    public ResponseEntity<PingResponse> ping() {
+        return ResponseEntity.ok(new PingResponse("ok", "1.0.0"));
     }
 }
